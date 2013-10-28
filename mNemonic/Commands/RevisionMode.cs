f7f0a0dfe3313e53bd96809178508b3d27faebd3
manyrootsofallevil <manyrootsofallevil@gmail.com>
2013-10-28 20:39:48 +0000

    Revision Mode vs Regular Mode wired up.

---
+++ mNemonic/Commands/RevisionMode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace mNemonic.Commands
+{
+    public class RevisionMode : ICommand
+    {
+        System.Windows.Forms.Timer Timer;
+
+        public void Execute(object parameter)
+        {
+            Timer = (System.Windows.Forms.Timer)App.Current.FindResource("Timer");
+
+            if (Boolean.Parse(parameter.ToString()))
+            {
+                Timer.Interval = 250;
+            }
+            else
+            {
+                Timer.Interval = Int32.Parse(ConfigurationManager.AppSettings["Interval"]) * 1000 * 60;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
