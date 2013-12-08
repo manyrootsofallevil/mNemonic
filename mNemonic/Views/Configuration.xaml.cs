9d4915e0331b0e96fd61184479ffac05e0fe9e1d
manyrootsofallevil <manyrootsofallevil@gmail.com>
2013-12-08 18:00:05 +0000

    Added a bit of logging

--- mNemonic/Views/Configuration.xaml.cs
+++ mNemonic/Views/Configuration.xaml.cs
@@ -49,31 +49,38 @@ namespace mNemonic.Forms
         {
             int interval, initialInterval;
 
-            if (Int32.TryParse(Interval.Text.Trim(), out interval) &&
-                Int32.TryParse(InitialInterval.Text.Trim(), out initialInterval))
+            try
             {
-                // Get the current configuration file.
-                System.Configuration.Configuration config =
-                        ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                if (Int32.TryParse(Interval.Text.Trim(), out interval) &&
+            Int32.TryParse(InitialInterval.Text.Trim(), out initialInterval))
+                {
+                    // Get the current configuration file.
+                    System.Configuration.Configuration config =
+                            ConfigurationManager.OpenExeConfiguration(System.Reflection.Assembly.GetExecutingAssembly().Location);
 
-                config.AppSettings.Settings["Interval"].Value = interval.ToString();
-                config.AppSettings.Settings["CollectStats"].Value = CollectStats.IsChecked.ToString();
-                config.AppSettings.Settings["InitialInterval"].Value = initialInterval.ToString();
+                    config.AppSettings.Settings["Interval"].Value = interval.ToString();
+                    config.AppSettings.Settings["CollectStats"].Value = CollectStats.IsChecked.ToString();
+                    config.AppSettings.Settings["InitialInterval"].Value = initialInterval.ToString();
 
-                //May as well update the time here.
-                Timer = (System.Windows.Forms.Timer)App.Current.FindResource("Timer");
-                Timer.Interval = interval * 60 * 1000;
+                    //May as well update the time here.
+                    Timer = (System.Windows.Forms.Timer)App.Current.FindResource("Timer");
+                    Timer.Interval = interval * 60 * 1000;
 
-                config.Save(ConfigurationSaveMode.Full);
-                ConfigurationManager.RefreshSection("appSettings");
+                    config.Save(ConfigurationSaveMode.Full);
+                    ConfigurationManager.RefreshSection("appSettings");
 
-                this.Close();
+                    this.Close();
 
+                }
+                else
+                {
+                    //Meaningful error message let your user become lazy. don't use them. keep then on their toes
+                    MessageBox.Show("An Error occurred. Try Again");
+                }
             }
-            else
-            {
-                //Meaningful error message let your user become lazy. don't use them. keep then on their toes
-                MessageBox.Show("An Error occurred. Try Again");
+            catch (Exception ex)
+            {//TODO: Why is this failing? Permissions? 
+                System.Diagnostics.EventLog.WriteEntry(Constants.source, "An error ocurred saving configuration settings. Exception: {0}", System.Diagnostics.EventLogEntryType.Error);
             }
         }
 
